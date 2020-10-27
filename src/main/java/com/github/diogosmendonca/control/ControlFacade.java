dff5f03b2513a5cd2df4e3092e81ab3109fcbaab
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-10-27 14:54:27 -0300

    Initial Commit

---
+++ src/main/java/com/github/diogosmendonca/control/ControlFacade.java
@@ -0,0 +1,13 @@
+package  com.github.diogosmendonca.control;
+
+import com.github.diogosmendonca.control.*;
+
+/**
+ * Develop your facade in this class.
+ * 
+ * You must NOT change the name of the class. 
+ */
+public class ControlFacade{
+
+
+}
